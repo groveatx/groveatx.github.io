86695862c46e2b44e564aa75094e72177f36a5f8
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-06-02 13:22:30 -0500

    build style.less to css/style.css

--- _plugins/less.rb
+++ _plugins/less.rb
@@ -12,36 +12,25 @@ module Jekyll
 
     def generate(site)
       src_root = site.config['source']
-      dest_root = site.config['destination']
       less_ext = /\.less$/i
       lessc_bin = site.config['lessc'] || 'lessc'
 
       sf = StaticFile.new(site, src_root, '_less', 'style.less')
-      site.static_files.delete(sf)
 
-      less_path = sf.path
-      css_path = less_path
+      css_path = sf.path
                   .gsub(less_ext, '.css')
-                  .gsub(src_root, dest_root)
+                  .gsub(src_root + '/', '')
                   .gsub('_less', 'css')
-      relative_dir = File.dirname(css_path).gsub(dest_root, '')
       file_name = File.basename(css_path)
 
       FileUtils.mkdir_p(File.dirname(css_path))
 
       begin
-        command = [lessc_bin,
-                   less_path,
-                   css_path
-                   ].join(' ')
+        command = [lessc_bin, sf.path, css_path].join(' ')
         puts 'Compiling LESS: ' + command
         puts `#{command}`
         raise "LESS compilation error" if $?.to_i != 0
       end
-
-      # Add this output file so it won't be "cleaned away"
-      site.static_files << LessCssFile.new(
-        site, site.source, relative_dir, file_name)
     end
   end
 end
